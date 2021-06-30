69ae69b25f3a22df819d667892df8efd30add507
Anvar Narzullaev <anvarbek@gmail.com>
2021-06-30 15:53:24 +0800

    5-dars kodi qo'shildi.

---
+++ 04-BlogProject/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
+
+urlpatterns = [
+    path('post/<int:pk>/delete/',BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+]
